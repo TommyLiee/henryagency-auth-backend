09f2e754c2c62cc049b749b6989bce5428543847
TommyLiee <117362421+TommyLiee@users.noreply.github.com>
2025-07-11 21:53:16 +0200

    Order.js

--- models/Order.js
+++ models/Order.js
@@ -3,12 +3,12 @@ const mongoose = require("mongoose");
 const orderSchema = new mongoose.Schema({
   userId: String,
   email: String,
-  title: String, // facultatif
+  title: String, // facultatif, peut être utilisé pour résumé global
   swissLink: String,
   status: { type: String, default: "en attente" },
   date: { type: Date, default: Date.now },
   messages: [],
-  items: [ // ✅ NOUVEAU
+  items: [ // ✅ liste claire des vidéos commandées
     {
       name: String,
       quantity: Number,
