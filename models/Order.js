4bdafdbc75c868e1805001f2b30c5ccad6440430
TommyLiee <117362421+TommyLiee@users.noreply.github.com>
2025-07-11 16:42:41 +0200

    Order.js

--- models/Order.js
+++ models/Order.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  email: String,
-  title: String,
-  swissLink: String,
+  email: { type: String, required: true },
+  title: { type: String, required: true },
+  swissLink: { type: String },
+  total: { type: Number, required: true },
   status: { type: String, default: 'en attente' },
   date: { type: Date, default: Date.now },
   messages: [
