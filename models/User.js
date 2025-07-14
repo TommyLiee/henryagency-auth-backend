35bfbc1278eb5fce1ebae176f357e032e39adbb2
TommyLiee <117362421+TommyLiee@users.noreply.github.com>
2025-07-14 10:17:38 +0200

    User.js

--- models/User.js
+++ models/User.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false }, // ✅ Devient optionnel pour les comptes Google
   firstName: { type: String },
   lastName: { type: String },
-  phone: { type: String }
+  phone: { type: String },
+  provider: { type: String, default: "local" } // 🧠 Bonus : permet de savoir si c'est un user "google" ou "local"
 });
 
 // ✅ Évite l'erreur "OverwriteModelError"
